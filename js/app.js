9523ff8ade0d2d650e532311c7767c7d51cdebf9
Agustina Vega <avegamaiquez05@gmail.com>
2022-10-23 10:55:10 -0300

    Calculadora estructurada

---
+++ js/app.js
@@ -0,0 +1,8 @@
+function calcReglaDeTres (valor1, valor2, valor3) {
+    var n1 = parseInt(document.getElementById(valor1).value);
+    var n2 = parseInt(document.getElementById(valor2).value);
+    var n3 = parseInt(document.getElementById(valor3).value);
+    let resultado = (n2*n3)/n1
+    document.getElementById("resultado").innerHTML = p ;
+}
+
