09578f48d5862c774c45d49213ed4b8b69c0b31b
Agustina Vega <avegamaiquez05@gmail.com>
2022-10-23 17:53:52 -0300

    Nosotros, Calculadora y ccs

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@ function calcReglaDeTres (valor1, valor2, valor3) {
     var n2 = parseInt(document.getElementById(valor2).value);
     var n3 = parseInt(document.getElementById(valor3).value);
     let resultado = (n2*n3)/n1
-    document.getElementById("resultado").innerHTML = p ;
+    document.getElementById("resultado").innerHTML = text;
 }
+const buttonSubmit = document.querySelector("#buttonSubmmit")
+const resultadoRegla = document.querySelector("#resultado")
 
+const
