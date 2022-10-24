471934a31a22a180bc8f76c99512a44600b61330
Agustina Vega <avegamaiquez05@gmail.com>
2022-10-24 02:28:32 -0300

    calcladora casi lista y página terminada

--- js/app.js
+++ js/app.js
@@ -27,3 +27,10 @@ const realizarcalc = ()=> {
 
 } 
 */
+document.querySelector('#calcular').addEventListener('click', () =>{
+    const n2 = parseInt(document.querySelector('#n2').value);
+    const n1 = parseInt(document.querySelector('#n1').value);
+    const n3 = parseInt(document.querySelector('#n3').value);
+    let r = (n2*n3)/n1
+    document.querySelector('#r2').innerHTML = r;
+})
